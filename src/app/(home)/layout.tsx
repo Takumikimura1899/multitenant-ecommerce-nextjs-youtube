b3b881fccbe89334fec299a8efcd94b41c1a0a0c
Takumikimura1899 <kimura141899@gmail.com>
2025-05-07 07:17:39 +0900

    about footer

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,12 @@
+import { Footer } from './Footer';
 import { Navbar } from './Navbar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
-
-      {children}
+      <div className='flex-1 bg-[#f4f4f0]'>{children}</div>
+      <Footer />
     </div>
   );
 };
