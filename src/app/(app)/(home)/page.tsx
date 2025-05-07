6d01b81a5432cebc64bb24f5785fc9da03eec042
Takumikimura1899 <kimura141899@gmail.com>
2025-05-08 07:05:10 +0900

    category-subcategories db:migrate

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -3,8 +3,25 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Progress } from '@/components/ui/progress';
 import { Textarea } from '@/components/ui/textarea';
+import { getPayload } from 'payload';
+import configPromise from '@payload-config';
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  console.log('data', data);
   return (
     <div className='p-4'>
       <div className='flex flex-col gap-y-4'>
