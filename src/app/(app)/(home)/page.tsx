50f331f3782a57eaa3949b0132893de720b167d3
Takumikimura1899 <kimura141899@gmail.com>
2025-06-02 07:40:03 +0900

    trpc router render page

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,29 +1,7 @@
-import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
-import { Input } from '@/components/ui/input';
-import { Progress } from '@/components/ui/progress';
-import { Textarea } from '@/components/ui/textarea';
+import { getQueryClient, trpc } from '@/trpc/server';
 
 export default async function Home() {
-  return (
-    <div className='p-4'>
-      <div className='flex flex-col gap-y-4'>
-        <div>
-          <Button variant='elevated'>Button</Button>
-        </div>
-        <div>
-          <Input placeholder='Input' />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea placeholder={'text-area'} />
-        </div>
-        <div>
-          <Checkbox />
-        </div>
-      </div>
-    </div>
-  );
+  const categories = await trpc.categories.getMany();
+
+  return <div>{JSON.stringify(categories, null, 2)}</div>;
 }
