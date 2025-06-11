87967655f9a1901a7bf427c4a5d74603544948b3
Takumikimura1899 <kimura141899@gmail.com>
2025-06-12 08:14:13 +0900

    trpc user procedure

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,8 @@
-export default async function Home() {
-  return <div>Home</div>;
+'use client';
+
+import { trpc } from '@/trpc/client';
+
+export default function Home() {
+  const data = trpc.auth.session.useQuery();
+  return <div>{JSON.stringify(data.data?.user, null, 2)}</div>;
 }
---
+++ src/modules/auth/server/procedures.ts
@@ -0,0 +1,14 @@
+import { headers as getHeaders } from 'next/headers';
+
+import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+import { Category } from '@/payload-types';
+
+export const authRouter = createTRPCRouter({
+  session: baseProcedure.query(async ({ ctx }) => {
+    const headers = await getHeaders();
+
+    const session = await ctx.db.auth({ headers });
+
+    return session;
+  }),
+});
